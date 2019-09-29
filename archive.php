203538fcfeae7b59c8cd09dee63dec6d23b5a8d5
yjolivier <bouxouyao@gmail.com>
2019-09-29 13:35:46 +0000

    Rebase stopping

--- archive.php
+++ archive.php
@@ -1,33 +1,71 @@
-<?php get_header(); ?>
-			<section class="row section">
-				<div class="archive-section col-lg-12">
-					<div class="container archive-title-container">
-						<div class="row archive-title">
-				            <h2>Page Title</h2>
-				        </div>
-				        <div class="row space"></div>
-			        </div>
+<?php
+/*
+=========================================================================================
+@package        ElogeBeonao
+@link           https://codex.wordpress.org/Pages
+@copyright      Copyright (C) 2017. Samuel Guebo
+@license        GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+@author         Samuel Guebo (https://github.com/samuelguebo/), Yao Olivier (https://github.com/yjolivier/)
+=========================================================================================
+*/
+
+get_header(); ?>
+			<section class="row page-content">
+				<div class="single-slide col-lg-12">
+					<?php get_template_part( 'page-templates/page-slide');?>
 				</div>
-				<div class="container block-archive-container">
-			        <div class="row archive-article">
-			        	<div class="card block-archiver col-lg-8">
-				            <div class="card-img">
-				            	<img src="img/img4.jpg" class="card-img-top" alt="...">
-				            </div>
-				            <div class="card-body">
-				                <p class="card-text">
-				                    <h5>EVENEMENT</h5>
-				          			<p>Discover flexible car financing options for new or used cars, and specialty vehicles nsequat fringi porta.</p>
-				                </p>
-				                <div class="btn-article">
-				                	<a href="#">SAVOIR PLUS</a>
-				                </div>
-				            </div>
+				<div class="container single-content">
+			        <div class="row sidbar-article-container">
+			        	<div class="card single-article col-lg-8 col-md-12">
+				            <div class="row article">
+				            	<!-- Debut de la boucle -->
+								<?php if (have_posts()) : 
+									$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+									?>
+						            <?php while (have_posts()) : the_post(); ?>
+							        	<div class="archive-block col-lg-6">
+								        	<div class="archive-article-box">  
+									            <div class="card-img">
+									            	<?php
+								                        if ( has_post_thumbnail_or_image ()) { 
+								                            the_post_thumbnail( "post-thumb" ); 
+								                        }
+								                    ?>
+									            </div>
+									            <div class="card-body">
+									                <p class="card-text">
+									                    <h5><?php the_title();?></h5>
+									          			<p><?php jesussauve_excerpt(100);?></p>
+									                </p>
+									                <center>
+										                <div class="btn-article">
+										                	<a href="<?php the_permalink();?>"><?php _e('Savoir plus','elogebeonao')?></a>
+										                </div>
+									                </center>
+									            </div>
+									        </div>
+							            </div>
+											<?php endwhile;?>
+											
+											<?php 
+												else : get_template_part( 'template-parts/contents', '404' );
+												endif;
+											?>
+					        </div>
+					        <div class="pagination-wrapper" >
+					            <?php the_posts_pagination( array(
+					                'mid_size' => 2,
+					                'prev_text' => __( '&laquo;', 'elogebeonao' ),
+					                'next_text' => __( '&raquo;', 'elogebeonao' ),
+					                'screen_reader_text' => ' '
+					            ) ); ?>
+					        </div> 
 			            </div>
 			            <div class="col-lg-4 sidebar">
-			            	bonjour le monde
+			            	<?php get_sidebar(); ?>
 			            </div>
 			        </div>
 				</div>
 			</section>
 <?php get_footer(); ?>
+				
