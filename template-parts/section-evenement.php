d565dc5d20ec3499fcaa47e136e4af34a7b20230
yjolivier <bouxouyao@gmail.com>
2020-08-09 11:46:47 +0000

    [IMPR] Section-evenement style

--- template-parts/section-evenement.php
+++ template-parts/section-evenement.php
@@ -4,25 +4,29 @@
   $evenement_section_category = esc_html(get_theme_mod('evenement_section_category'));
 ?>
 <section class="section-evenement row">
-  <?php
-    if ( have_posts() ) :
-      // custom params for my loop
-      $args = array(
-          'cat' => $evenement_section_category, // category slug
-          'posts_per_page' => 3, // how many articles to display
-      );
-      $evenements_posts = new WP_Query($args);
-  ?>
-  <div class="section-evenement-title col-md-12 col-lg-12 col-sm-12">
-    <h2><?php echo $evenement_section_title;?></h2>
-    <p><?php echo $evenement_section_subtitle;?></p>
+  <div class="container">
+    <div class="row">
+      <?php
+        if ( have_posts() ) :
+          // custom params for my loop
+          $args = array(
+              'cat' => $evenement_section_category, // category slug
+              'posts_per_page' => 3, // how many articles to display
+          );
+          $evenements_posts = new WP_Query($args);
+      ?>
+      <div class="section-evenement-title col-md-12 col-lg-12 col-sm-12">
+        <h2><?php echo $evenement_section_title;?></h2>
+        <p><?php echo $evenement_section_subtitle;?></p>
+      </div>
+      <div class="row block-article col-md-12 col-lg-12 col-sm-12">
+        <?php 
+          while ( $evenements_posts->have_posts() ) : $evenements_posts->the_post(); 
+            get_template_part('template-parts/content','article');
+          endwhile; wp_reset_postdata();
+        ?>
+      </div>
+      <?php endif;?> 
+    </div>
   </div>
-  <div class="row block-article col-md-12 col-lg-12 col-sm-12">
-    <?php 
-      while ( $evenements_posts->have_posts() ) : $evenements_posts->the_post(); 
-        get_template_part('template-parts/content','article');
-      endwhile; wp_reset_postdata();
-    ?>
-  </div>
-  <?php endif;?> 
 </section>
